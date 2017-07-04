2a6ad5d8eed57b16449d1967dbba3f437f6eed6e
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-07-04 16:31:57 +0100

    Helper for use on teamcity

---
+++ docker/build-ci.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+GIT_ID=$(git rev-parse --short HEAD)
+GIT_BRANCH=$(git symbolic-ref --short HEAD)
+REGISTRY=docker.montagu.dide.ic.ac.uk:5000
+NAME=orderly
+
+APP_DOCKER_TAG=$REGISTRY/$NAME
+APP_DOCKER_COMMIT_TAG=$REGISTRY/$NAME:$GIT_ID
+APP_DOCKER_BRANCH_TAG=$REGISTRY/$NAME:$GIT_BRANCH
+
+docker build \
+       --tag $APP_DOCKER_COMMIT_TAG \
+       --tag $APP_DOCKER_BRANCH_TAG \
+       -f docker/Dockerfile .
+docker push $APP_DOCKER_BRANCH_TAG
+docker push $APP_DOCKER_COMMIT_TAG
