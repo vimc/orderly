402347c9f03fe6f340340e6a9d5dec3b8cdbcb05
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-07-13 17:37:08 +0100

    Build demo zip file as teamcity target

---
+++ docker/teamcity-demo.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+TAG=$(git rev-parse --short HEAD)
+DEST=demo
+docker run --rm --entrypoint create_orderly_demo.sh \
+       -u "${UID}" \
+       -v "${PWD}":/orderly \
+       -w /orderly \
+       "docker.montagu.dide.ic.ac.uk:5000/orderly:${TAG}" \
+       "$DEST"
+zip -r $DEST.zip $DEST
+rm -r $DEST
