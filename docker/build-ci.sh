d380d495970361f18a1a75ddecaf035cba350830
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-08-18 13:33:26 +0100

    Inject orderly version information into docker image

--- docker/build-ci.sh
+++ docker/build-ci.sh
@@ -11,8 +11,11 @@ APP_DOCKER_COMMIT_TAG=$REGISTRY/$NAME:$GIT_ID
 APP_DOCKER_BRANCH_TAG=$REGISTRY/$NAME:$GIT_BRANCH
 
 docker build \
+       --build-arg GIT_ID=$GIT_ID \
+       --build-arg GIT_BRANCH=$GIT_BRANCH \
        --tag $APP_DOCKER_COMMIT_TAG \
        --tag $APP_DOCKER_BRANCH_TAG \
        -f docker/Dockerfile .
+
 docker push $APP_DOCKER_BRANCH_TAG
 docker push $APP_DOCKER_COMMIT_TAG
