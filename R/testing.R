a2cbde032167d7f6077336eac3f40156a4dc7ab3
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-07-25 14:42:39 +0100

    Allow other examples to be driveable like the demo

--- R/testing.R
+++ R/testing.R
@@ -4,7 +4,11 @@
 ## See inst/demo/demo.yml for more information.
 create_orderly_demo <- function(path = tempfile()) {
   prepare_orderly_example("demo", path)
+  run_orderly_demo(path)
+}
 
+
+run_orderly_demo <- function(path) {
   dat <- read_demo_yml(path)
 
   orderly_default_config_set(orderly_config(path))
