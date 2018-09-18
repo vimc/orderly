bb6c8d16341700affdd977d6dec584a55b674560
JamesThompson1729 <james.thompson@imperial.ac.uk>
2018-09-18 10:07:45 +0100

    Tidy up the code + add report id to message

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,82 +1,112 @@
-get_dependencies <- function(name, id = NULL, config = NULL, draft = FALSE) {
-#  print(sprintf("%s, %s", "get_dependencies", config))
+##' @title Get the dependencies for a given report from the orderly_run.yml
+##' 
+##' @param report the name of the report
+##' @param id the id of the report, if omitted, use the id of the latest report
+##' @param config_path Path to the orderly configuration
+##' @param draft If true look in the draft directory otehrwise look in archive
+##'
+##' @export
+get_dependencies <- function(name, id = NULL, config_path = NULL, draft = FALSE) {
   if (is.null(id))
-    id <- orderly:::orderly_latest(name, config = config, draft = draft)
+    id <- orderly:::orderly_latest(name, config = config_path, draft = draft)
   
-  if (is.null(config))
-    config <- "."
+  if (is.null(config_path))
+    config_path <- "."
 
   if (draft)
-    rds_path <- file.path(config, "draft", name, id, "orderly_run.rds")
+    rds_path <- file.path(config_path, "draft", name, id, "orderly_run.rds")
   else
-    rds_path <- file.path(config, "archive", name, id, "orderly_run.rds")
+    rds_path <- file.path(config_path, "archive", name, id, "orderly_run.rds")
 
   if (!file.exists(rds_path)) {
-    print(rds_path)
-    warning(sprintf("The report %s:%s does not exist or does not have orderly_run.rds", name, id))
+    warning(sprintf(
+      "The report %s:%s does not exist or does not have orderly_run.rds",
+      name, id))
     return(NULL)
   }
   run_data <- readRDS(rds_path)
   return(run_data$meta$depends)
 }
-##' @title gneerate the dependency tree for a given report
+
+##' @title Generate a list of dependencies for a given report
 ##'
 ##' @param report the name of the report
 ##' @param id the id of the report, if omitted, use the id of the latest report
+##' @param config_path Path to the orderly configuration
 ##' @param draft If true look in the draft directory otehrwise look in archive
 ##' @param depth only used internally, to prevent infinite recursion and make 
 ##'              the output log look nice
-##' @param dependent_reports the list of reports, needs to passed as an argument
+##' @param dep_reports the list of reports, needs to passed as an argument
 ##'                          (and returned)
 ##'
 ##' @export
-what_depends_on <- function(report, id = "latest", config = NULL,
+what_depends_on <- function(report, id = "latest", config_path = NULL,
                             draft = FALSE, depth = 0,
-                            dependent_reports = list()) {
-#  print(sprintf("%s, %s", "what_depends_on", config))
+                            dep_reports = list()) {
+  # this is potentially needed to trap circular dependencies
+  # TODO - We could be clever here and check if this report already appears in
+  # dep_reports
   if (depth > 10) {
     orderly::orderly_log("depends",
                          "*WARNING* There appears to be a circular dependency")
-    return(dependent_reports)
+    return(dep_reports)
   }
   
+  # we need to go find the latest version of the report
   if (id == "latest")
-    id <- orderly:::orderly_latest(report, config = config, draft = draft)
+    id <- orderly_latest(report, config = config_path, draft = draft)
   
+  # get a list of all local reports - doing this every time is tedious, but
+  # it doesn't take long and looks nicer than passing around a vector of names
   if (draft)
-    rep_names <- unique(orderly::orderly_list_drafts(config = config)$name)
+    rep_names <- unique(orderly_list_drafts(config = config_path)$name)
   else
-    rep_names <- unique(orderly::orderly_list_archive(config = config)$name)
+    rep_names <- unique(orderly_list_archive(config = config_path)$name)
   
+  # here's the plan - We have a report, A.
+  # - We iterate through the list of local reports reading the dependencies from
+  # the orderly_run.yml
+  # - If for some report (B), A appears in the list of dependencies of B, then
+  # B depends on A so this gets added to the list
+  # - Then we need to check which reports depend on B
   for (name in rep_names) {
-    rep_id <- orderly:::orderly_latest(name, config = config, draft = draft)
-    deps <- get_dependencies(name = name, id = rep_id, draft = draft, config = config)
-    if (report %in% deps$name) { ## need to check
+    rep_id <- orderly_latest(name, config = config_path, draft = draft)
+    deps <- get_dependencies(name = name, id = rep_id, draft = draft,
+                             config_path = config_path)
+    if (report %in% deps$name) {
       i <- which(deps$name == report)
-      dependent_reports[[length(dependent_reports) + 1]] <- 
-        list(name = name, depth = depth)
-      dependent_reports <- what_depends_on(report = name, id = deps$id[i], config = config,
-                                           draft = draft, depth = depth + 1,
-                                           dependent_reports = dependent_reports)
+      dep_reports[[length(dep_reports) + 1]] <-  list(name = name,
+                                                      id = rep_id,
+                                                      depth = depth)
+      dep_reports <- what_depends_on(report = name, id = rep_id, 
+                                     config_path = config_path,
+                                     draft = draft, depth = depth + 1,
+                                     dep_reports = dep_reports)
     }
   }
-  return(dependent_reports)
+  return(dep_reports)
 }
 
-##' @title Print the dependency tree with orderly log
-##' @param dep_tree a list of dependencies and their depth in the dependcy tree
+##' @title Print the dependency tree for a given report using orderly log
+##' 
+##' @param report the name of the report
+##' @param id the id of the report, if omitted, use the id of the latest report
+##' @param config_path Path to the orderly configuration
+##' @param draft If true look in the draft directory otehrwise look in archive
 ##'
 ##' @export
 print_dep_tree <- function(report, id = "latest", draft = FALSE,
-                           config = NULL) {
-#  print(sprintf("%s, %s", "print_dep_tree", config))
+                           config_path = NULL) {
   dep_tree <- what_depends_on(report = report, id = id, draft = draft, 
-                              config = config)
+                              config_path = config_path)
   if (length(dep_tree) == 0)
     orderly::orderly_log("dep tree", "Nothing to update.")
   
   for (dep in dep_tree) {
-    dep_str <- sprintf("%s- %s", paste(rep("  ", dep$depth), collapse = ""), dep$name)
+    dep_str <- sprintf("%s- %s [%s]",
+                       paste(rep("  ", dep$depth), collapse = ""), 
+                       dep$name,
+                       dep$id)
     orderly::orderly_log("depends", dep_str)
   }
 } 
--- tests/testthat/test-dep-tree.R
+++ tests/testthat/test-dep-tree.R
@@ -7,30 +7,38 @@ test_that("no dependencies", {
   orderly_run("example", config = path, id_file = tmp, echo = FALSE)
 
   messages <- capture_messages(
-    print_dep_tree("example", draft = TRUE, config = path)
+    print_dep_tree("example", draft = TRUE, config_path = path)
   )
   expect_equal(messages, "[ dep tree   ]  Nothing to update.\n")
 })
 
-# check reports with dependencies
+# check reports with recursive dependencies
 # example
 # - depend
 #   - depend3
 test_that("has dependencies", {
   path <- prepare_orderly_example("depends")
   tmp <- tempfile()
-  orderly_run("example", config = path, id_file = tmp, echo = FALSE)
-  orderly_run("depend", config = path, id_file = tmp, echo = FALSE)
-  orderly_run("depend3", config = path, id_file = tmp, echo = FALSE)
+  r1 <- orderly_run("example", config = path, id_file = tmp, echo = FALSE)
+  r2 <- orderly_run("depend", config = path, id_file = tmp, echo = FALSE)
+  r3 <- orderly_run("depend3", config = path, id_file = tmp, echo = FALSE)
+  
+  print(r1)
   
   messages <- capture_messages(
-    print_dep_tree("example", draft = TRUE, config = path)
+    print_dep_tree("example", draft = TRUE, config_path = path)
   )
-  expect_equal(messages, c("[ depends    ]  - depend\n",
-                           "[ depends    ]    - depend3\n"))
+  
+  exp_message <- c(sprintf("[ depends    ]  - depend [%s]\n", r2),
+                   sprintf("[ depends    ]    - depend3 [%s]\n", r3))
+  
+  expect_equal(messages, exp_message)
   
   messages <- capture_messages(
-    print_dep_tree("depend", draft = TRUE, config = path)
+    print_dep_tree("depend", draft = TRUE, config_path = path)
   )
-  expect_equal(messages, "[ depends    ]  - depend3\n")
+  
+  exp_message <- sprintf("[ depends    ]  - depend3 [%s]\n", r3)
+  
+  expect_equal(messages, exp_message)
 })
