3e931378da9e72e25737e5ee20fd8ca210b24bdb
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-11-20 15:12:43 +0000

    Test through db2 bits that mirror db1 paths

--- tests/testthat/test-db.R
+++ tests/testthat/test-db.R
@@ -15,6 +15,7 @@ test_that("custom fields", {
   on.exit(DBI::dbDisconnect(con))
 
   expect_true(DBI::dbExistsTable(con, "orderly"))
+  expect_true(DBI::dbExistsTable(con, "orderly_schema"))
 
   ## TODO: should the db initialisation here check that the custom
   ## fields are all OK?  But that will happen rather a lot and that's
@@ -23,15 +24,26 @@ test_that("custom fields", {
   config <- orderly_config_get(path)
   expect_error(report_db_init(con, config, TRUE),
                "Table 'orderly' already exists")
+  expect_error(report_db2_init(con, config, TRUE),
+               "Table 'orderly_schema' already exists")
 
   d <- DBI::dbReadTable(con, "orderly")
   d <- d[setdiff(names(d), "author")]
   DBI::dbWriteTable(con, "orderly", d, overwrite = TRUE)
+
+  d <- DBI::dbReadTable(con, "report_version")
+  d <- d[setdiff(names(d), "author")]
+  DBI::dbWriteTable(con, "report_version", d, overwrite = TRUE)
+
   expect_error(report_db_init(con, config, FALSE),
+               "custom fields 'author' not present in existing database")
+  expect_error(report_db2_init(con, config, FALSE),
                "custom fields 'author' not present in existing database")
 
   config$fields <- NULL
   expect_error(report_db_init(con, config, FALSE),
+               "custom fields 'requester', 'comments' in database")
+  expect_error(report_db2_init(con, config, FALSE),
                "custom fields 'requester', 'comments' in database")
 })
 
@@ -71,10 +83,12 @@ test_that("no transient db", {
 
 test_that("db includes parameters", {
   path <- prepare_orderly_example("example")
-  id <- orderly_run("example", parameters = list(cyl = 4), config = path)
+  id <- orderly_run("example", parameters = list(cyl = 4), config = path,
+                    echo = FALSE)
   orderly_commit(id, config = path)
   con <- orderly_db("destination", config = path)
   d <- DBI::dbReadTable(con, "parameters")
+  DBI::dbDisconnect(con)
   expect_equal(d, data_frame(report_version = id,
                              name = "cyl",
                              type = "number",
