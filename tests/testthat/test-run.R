af2ef02b1e406849eed4cad82b1ff282786c68fe
james thompson <james.thompson@imperial.ac.uk>
2019-01-31 10:55:38 +0000

    add test for missing README.md

--- tests/testthat/test-run.R
+++ tests/testthat/test-run.R
@@ -708,7 +708,7 @@ test_that("auto copy README.md",  {
   expect_true(file.exists(file.path(p, "README.md")))
 })
 
-test_that("yaml copy README.md ",  {
+test_that("yaml copy README.md",  {
   path <- prepare_orderly_example("minimal")
   ## in report directory create a file called README.md
   path_example <- file.path(path, "src", "example")
@@ -723,6 +723,21 @@ test_that("yaml copy README.md ",  {
   id <- orderly_run("example", config = path, echo = FALSE)
   p <- file.path(path, "draft", "example", id)
   expect_true(file.exists(file.path(p, "README.md")))
+})
+
+test_that("yaml copy README.md missing",  {
+  path <- prepare_orderly_example("minimal")
+  ## in report directory create a file called README.md
+  path_example <- file.path(path, "src", "example")
+  
+  yml_path <- file.path(path_example, "orderly.yml")
+  minimal_yml <- readLines(yml_path)
+  extended_yml <- c(minimal_yml, sprintf("%s: %s", "has_readme", "TRUE"))
+  writeLines(extended_yml, yml_path)
+
+  
+  expect_error(orderly_run("example", config = path, echo = FALSE),
+               "README.md does not exist")
 })
 
 test_that("bad yaml copy README.md ",  {
