b6189b2cd29a805c81aecc063dcbedd4d3e76bf4
james thompson <james.thompson@imperial.ac.uk>
2019-07-29 16:23:24 +0100

    improve the appearance of the tree

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,11 +1,11 @@
 ##' @title Get the dependencies for a given report from the orderly_run.yml
 ##' 
-##' @param report the name of the report
+##' @param name the name of the report
 ##' @param id the id of the report, if omitted, use the id of the latest report
-##' @param draft If true look in the draft directory otehrwise look in archive
-##' @inheritParams orderly_list 
-##'
-##' @export
+##' @param root
+##' @param upstream
+##' @param locate
+##' @param con 
 get_dependencies_db <- function(name, id = NULL, root = NULL, upstream = FALSE,
                                 locate = TRUE, con = NULL) {
   ## get a connection to the database
@@ -90,7 +90,7 @@ is_latest_in_db <- function(con, id) {
   return(latest$id == id)
 }
 
-build_tree <- function(report, id = "latest", depth = 0, parent = NULL,
+build_tree <- function(name, id = "latest", depth = 0, parent = NULL,
                        tree = NULL, con = NULL, root = NULL, locate = TRUE,
                        upstream = FALSE) {
   if (depth > 10) {
@@ -104,27 +104,31 @@ build_tree <- function(report, id = "latest", depth = 0, parent = NULL,
   }
 
   # we need to go find the latest version of the report
-  latest_id <- get_latest_in_db(con, report)
+  latest_id <- get_latest_in_db(con, name)
   if (id == "latest") {
     id <- latest_id$id
+  } else {
+    if (name != id_to_name(con, id)) {
+      stop("id does not match report name")
+    }
   }
   
   ## if this is no tree, create a tree
   if (is.null(tree)) {
-    v <- Vertex$new(NULL, report, id, (id == latest_id$id))
+    v <- Vertex$new(NULL, name, id, (id == latest_id$id))
     tree <- Tree$new(v)
   } else {
-    v <- tree$add_child(parent, report, id, (id == latest_id$id))
+    v <- tree$add_child(parent, name, id, (id == latest_id$id))
   }
   
   ##children <- getchildren(report)
 
-  dep_ids <- get_dependencies_db(name = report, id = id,
+  dep_ids <- get_dependencies_db(name = name, id = id,
                                  root = root, locate = TRUE, con = con,
                                  upstream = upstream)
   for (dep_id in dep_ids) {
     dep_name <- id_to_name(id = dep_id, con = con)
-    build_tree(report = dep_name,
+    build_tree(name = dep_name,
                id = dep_id,
                depth = depth + 1,
                parent = v,
@@ -146,9 +150,9 @@ build_tree <- function(report, id = "latest", depth = 0, parent = NULL,
 ##' @inheritParams orderly_list 
 ##'
 ##' @export
-print_dep_tree <- function(report, id = "latest", draft = FALSE,
+print_dep_tree <- function(name, id = "latest", draft = FALSE,
                            root = NULL, locate = TRUE, remote = NULL,
-                           upstream = FALSE, con = NULL) {
+                           upstream = FALSE, con = NULL, propagate = FALSE) {
   if (is.null(con)) {
     con <- orderly_db("destination", orderly_config_get(root, locate))
     on.exit(DBI::dbDisconnect(con))
@@ -156,26 +160,45 @@ print_dep_tree <- function(report, id = "latest", draft = FALSE,
     #remote <- get_remote(remote, orderly_config_get(root, locate))
   }
 
-  dep_tree <- build_tree(report = report, id = id,
+  dep_tree <- build_tree(name = name, id = id,
                           root = root, locate = TRUE, con = con,
                           upstream = upstream)
-
-  library(crayon)
   if (upstream) {
     cat(yellow("++++++UPSTREAM++++++\n"))
   } else {
     cat(green("+++++DOWNSTREAM+++++\n"))
   }
 
-  # propegate out-of-date
-  #dep_tree <- propegate(dep_tree, )
+  # propagate out-of-date
+  if (propagate) {
+    propagate(dep_tree$root, upstream)
+  }
 
   if (length(dep_tree) == 0) {
     orderly_log("dep tree", "Nothing to update.")
   }
 
   dep_tree$print_tree()
-} 
+}
+
+
+propagate <- function(v, upstream) {
+  for (c in v$children) {
+    if (!upstream) {
+      if (!v$latest) {
+        c$latest = FALSE
+      }
+    }
+
+    propagate(c, upstream)
+
+    if (upstream) {
+      if (!c$latest) {
+        v$latest = FALSE
+      }
+    }
+  }
+}
 
 
 Vertex <- R6::R6Class("Vertex", list(
@@ -191,8 +214,8 @@ Vertex <- R6::R6Class("Vertex", list(
     self$id <- id
     self$latest <- latest
   },
-  add_child = function(child) {
-    self$children <- append(self$children, list(child))
+  add_child = function(c) {
+    self$children <- append(self$children, list(c))
   },
   to_string = function() {
     return(sprintf("%s [%s]", self$name, self$id))  
@@ -225,18 +248,31 @@ Tree <- R6::R6Class("Tree", list(
         return(v)
     }
   },
-  print_tree = function(v = self$root, depth = 0) {
+  # this is stupidly hacky to get the formatting right
+  print_tree = function(v = self$root, fvector = c()) {
     console_colour <- if (v$latest) {crayon::blue} else {crayon::red}
 
-    if (depth == 0) {
+    if (length(fvector) == 0) {
       cat(console_colour(sprintf("%s\n",v$to_string())))  
     } else {
+      spacing <- paste(ifelse(head(fvector, -1), "| ", "  "), collapse = "")
       cat(console_colour(sprintf("%s|___%s\n",
-                         strrep(x="| ", times = depth - 1),
+                         spacing,
                          v$to_string())))
     }
-    for (c in v$children) {
-      self$print_tree(c, depth + 1)
+
+    if (length(v$children) == 0) {
+      return()
+    }
+
+    for (i in 1:length(v$children)) {
+      c = v$children[[i]]
+
+      if (i != length(v$children)) {
+        self$print_tree(c, c(fvector, TRUE))
+      } else {
+        self$print_tree(c, c(fvector, FALSE))
+      }
     }
   }
   )
