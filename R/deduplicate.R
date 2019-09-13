849c112ff1fd03486ff3f5dc3af01ead5d9a66c2
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-09-13 13:33:39 +0100

    Support for deduplication

---
+++ R/deduplicate.R
@@ -0,0 +1,175 @@
+##' Deduplicate an orderly archive.  Deduplicating an orderly archive
+##' will replace all files that have the same content with "hard
+##' links".  This is reliable only on Unix-like systems.  If you use
+##' this feature, it is \emph{very important} that you treat your
+##' orderly archive as read-only (though you should be anyway) as
+##' changing one copy of a linked file changes all the other instances
+##' of it - the files are literally the same file.
+##'
+##' @title Deduplicate an orderly archive
+##'
+##' @inheritParams orderly_list
+##'
+##' @param dry_run Logical, indicating if the deduplication
+##'
+##' @return Invisibly, information about the duplication status of the
+##'   archive before deduplication was run.
+##' @export
+##' @examples
+##'
+##' path <- orderly::orderly_example("demo")
+##' id1 <- orderly::orderly_run("minimal", root = path)
+##' id2 <- orderly::orderly_run("minimal", root = path)
+orderly_deduplicate <- function(root = NULL, locate = TRUE, dry_run = TRUE, quiet = FALSE) {
+  config <- orderly_config_get(root, locate)
+  info <- orderly_deduplicate_info(config)
+  if (!quiet) {
+    print(info)
+  }
+  plan <- orderly_deduplicate_prepare(info)
+  if (!dry_run) {
+    orderly_deduplicate_run(plan)
+  }
+  invisible(info)
+}
+
+
+orderly_deduplicate_info <- function(config) {
+  con <- orderly_db("destination", config)
+  on.exit(DBI::dbDisconnect(con))
+
+  sql_file_info <- read_lines(orderly_file("database/file_info.sql"))
+  files <- DBI::dbGetQuery(con, sql_file_info)
+  files$path <- file.path(files$report,
+                          files$report_version,
+                          files$filename,
+                          fsep = "/")
+
+  paths <- file.path(config$root, "archive", files$path)
+  ## Information about the physical files, so we can work out which
+  ## files are already hardlinked
+  files <- cbind(
+    files,
+    fs::file_info(paths)[c("device_id", "inode", "hard_links")])
+  ## Information about if the files are unchanged
+  ## TODO: this step is slow for real files unfortunately
+  files$unchanged <- hash_files(paths, FALSE) == files$hash
+
+  ## Identify which files are duplicated
+  files$duplicated <-
+    files$hash %in% files$hash[duplicated(files$hash)]
+
+  ## Identify the first inode (in report id order)
+  i <- tapply(files$hash, files$hash)
+  files$inode_first <-
+    unname(tapply(files$inode, files$hash, function(x) x[[1L]]))[i]
+
+  ## Quick check:
+  stopifnot(all(vlapply(split(files, files$hash), function(x)
+    all(x$inode_first == x$inode[[1]]))))
+
+  ## Classify the files into different states
+  files$state <- rep("distinct", nrow(files))
+  files$state[files$duplicated] <- "duplicated"
+  files$state[files$duplicated & files$inode == files$inode_first] <- "linked"
+  files$state[files$duplicated & !duplicated(files$hash)] <- "distinct"
+
+  ## There is a corner case where if an input is an output (seen in
+  ## interactive in the demo) then we get into a right tangle.  So we
+  ## exclude the entry here for the artefact version of this.
+  files <- files[!duplicated(files$path), , drop = FALSE]
+  rownames(files) <- NULL
+
+  path_archive <- file.path(config$root, "archive")
+  all_files <- dir(path_archive, recursive = TRUE, all.files = TRUE,
+                   no.. = TRUE)
+  untracked <- setdiff(all_files, files$path)
+
+  exclude <- c("orderly_published.yml", ".orderly_archive_version", "README.md")
+  internal <- grepl("^orderly_run.*\\.(rds|yml)$", basename(untracked)) |
+    basename(untracked) %in% exclude
+
+  untracked <- data_frame(
+    path = untracked,
+    size = file_size(file.path(path_archive, untracked)),
+    internal = internal)
+
+  ret <- list(config = config,
+              files = files,
+              path = path_archive,
+              untracked = untracked)
+  class(ret) <- "orderly_deduplicate_info"
+  ret
+}
+
+
+orderly_deduplicate_prepare <- function(x) {
+  if (is_windows()) {
+    stop("Deduplication is not supported on windows")
+  }
+
+  assert_is(x, "orderly_deduplicate_info")
+
+  ## This is the only safe thing to do here:
+  if (length(unique(x$files$device_id)) > 1L) {
+    stop("Can't deduplicate as your orderly archive spans multiple devices")
+  }
+
+  ## Likely too strict as we can just skip these
+  if (!all(x$files$unchanged)) {
+    stop("Can't deduplicate files that have been modified")
+  }
+
+  ## Possibly too strict:
+  i <- x$files$state == "duplicated"
+  if (any(x$files$hard_links[i] > 1)) {
+    stop("Can't deduplicate files that have been hardlinked elsewhere")
+  }
+
+  from <- file.path(x$path, x$files$path[i])
+  to <- file.path(x$path,
+                  x$files$path[match(x$files$inode_first[i], x$files$inode)])
+  data_frame(from = from, to = to)
+}
+
+
+orderly_deduplicate_run <- function(x) {
+  for (i in seq_len(nrow(x))) {
+    relink(x$from[i], x$to[i])
+  }
+}
+
+
+##' @export
+format.orderly_deduplicate_info <- function(x, ...) {
+  dup <- x$files[x$files$state == "duplicated", ]
+  linked <- x$files[x$files$state == "linked", ]
+  untracked <- x$untracked[!x$untracked$internal, ]
+  c("Deduplication information for",
+    sprintf("  %s/archive", x$config$root),
+    sprintf("  - %d tracked files", nrow(x$files)),
+    sprintf("  - %s total size", pretty_bytes(sum(x$files$size))),
+    sprintf("  - %d duplicate files", nrow(dup)),
+    sprintf("  - %s duplicated size", pretty_bytes(sum(dup$size))),
+    sprintf("  - %d deduplicated files", nrow(linked)),
+    sprintf("  - %s deduplicated size", pretty_bytes(sum(linked$size))),
+    sprintf("  - %s untracked files", nrow(untracked)),
+    sprintf("  - %s untracked size", pretty_bytes(sum(untracked$size))))
+}
+
+
+##' @export
+print.orderly_deduplicate_info <- function(x, ...) {
+  cat(paste0(format(x), "\n", collapse = ""))
+  invisible(x)
+}
+
+
+relink <- function(from, to) {
+  backup <- paste0(from, ".bak")
+  fs::file_move(from, backup)
+  withCallingHandlers(
+    fs::link_create(to, from),
+    error = function(e) fs::file_move(backup, from))
+  fs::file_delete(backup)
+}
---
+++ tests/testthat/test-deduplicate.R
@@ -0,0 +1,167 @@
+context("deduplicate")
+
+test_that("deduplicate demo", {
+  skip_on_windows() # need inode information
+
+  path <- create_orderly_demo()
+  config <- orderly_config_get(path, locate)
+  x <- orderly_deduplicate_info(config)
+
+  expect_true(all(x$untracked$internal))
+  expect_true("orderly_run.rds" %in% basename(x$untracked$path))
+
+  repr <- format(x)
+  expect_match(repr, "- \\d{2,} tracked files", all = FALSE)
+  expect_match(repr, "- \\d{2,} duplicate files", all = FALSE)
+  expect_match(repr, "- 0 deduplicated files", all = FALSE)
+  expect_match(repr, "- 0 B deduplicated size", all = FALSE)
+  expect_match(repr, "- 0 untracked files", all = FALSE)
+  expect_match(repr, "- 0 B untracked size", all = FALSE)
+
+  expect_output(res <- withVisible(print(x)),
+                paste0(repr, collapse = "\n"),
+                fixed = TRUE)
+  expect_equal(res, list(value = x, visible = FALSE))
+
+  plan <- orderly_deduplicate_prepare(x)
+  expect_is(plan, "data.frame")
+  expect_equal(names(plan), c("from", "to"))
+
+  ## This will need updating every time we change the demo pretty
+  ## much.
+  expect_equal(nrow(plan), 43)
+
+  orderly_deduplicate_run(plan)
+
+  y <- orderly_deduplicate_info(config)
+  expect_equal(sum(y$files$state == "linked"), 43)
+  expect_equal(sum(y$files$state == "duplicated"), 0)
+  expect_equal(x$files$state == "distinct",
+               y$files$state == "distinct")
+})
+
+
+test_that("simpler use", {
+  skip_on_windows()
+  path <- orderly_example("demo")
+  id1 <- orderly_run("minimal", root = path, echo = FALSE)
+  id2 <- orderly_run("minimal", root = path, echo = FALSE)
+  id3 <- orderly_run("minimal", root = path, echo = FALSE)
+  orderly_commit(id1, root = path)
+  orderly_commit(id2, root = path)
+  info1 <- orderly_deduplicate(path, dry_run = FALSE, quiet = TRUE)
+  info2 <- orderly_deduplicate(path, dry_run = FALSE, quiet = TRUE)
+  orderly_commit(id3, root = path)
+  info3 <- orderly_deduplicate(path, dry_run = FALSE, quiet = TRUE)
+  info4 <- orderly_deduplicate(path, dry_run = FALSE, quiet = TRUE)
+
+  expect_equal(sum(info1$files$state == "duplicated"), 3)
+  expect_equal(sum(info2$files$state == "duplicated"), 0)
+  expect_equal(sum(info3$files$state == "duplicated"), 3)
+  expect_equal(sum(info4$files$state == "duplicated"), 0)
+
+  expect_equal(sum(info1$files$state == "linked"), 0)
+  expect_equal(sum(info2$files$state == "linked"), 3)
+  expect_equal(sum(info3$files$state == "linked"), 3)
+  expect_equal(sum(info4$files$state == "linked"), 6)
+})
+
+
+test_that("deduplicate empty", {
+  path <- orderly_example("demo")
+  x <- orderly_deduplicate_info(orderly_config(path))
+  expect_is(x, "orderly_deduplicate_info")
+  expect_equal(nrow(x$files), 0)
+  expect_equal(orderly_deduplicate(path, quiet = TRUE), x)
+})
+
+
+test_that("don't deduplicate across filesystem boundaries", {
+  skip_on_windows()
+  path <- orderly_example("demo")
+  id1 <- orderly_run("minimal", root = path, echo = FALSE)
+  id2 <- orderly_run("minimal", root = path, echo = FALSE)
+  orderly_commit(id1, root = path)
+  orderly_commit(id2, root = path)
+  info <- orderly_deduplicate_info(orderly_config(path))
+  info$files$device_id[[2]] <- info$files$device_id[[1]] + 1
+  expect_error(
+    orderly_deduplicate_prepare(info),
+    "Can't deduplicate as your orderly archive spans multiple devices")
+})
+
+
+test_that("don't deduplicate hardlinked files", {
+  skip_on_windows()
+  path <- orderly_example("demo")
+  id1 <- orderly_run("minimal", root = path, echo = FALSE)
+  id2 <- orderly_run("minimal", root = path, echo = FALSE)
+  orderly_commit(id1, root = path)
+  orderly_commit(id2, root = path)
+  info <- orderly_deduplicate_info(orderly_config(path))
+  info$files$hard_links[[5]] <- 2
+  expect_error(
+    orderly_deduplicate_prepare(info),
+    "Can't deduplicate files that have been hardlinked elsewhere")
+})
+
+
+test_that("don't deduplicate after file modification", {
+  skip_on_windows()
+  path <- orderly_example("demo")
+  id1 <- orderly_run("minimal", root = path, echo = FALSE)
+  id2 <- orderly_run("minimal", root = path, echo = FALSE)
+  orderly_commit(id1, root = path)
+  orderly_commit(id2, root = path)
+
+  ## truncate the file
+  file.create(file.path(path, "archive", "minimal", id1, "script.R"))
+
+  info <- orderly_deduplicate_info(orderly_config(path))
+  expect_equal(sum(!info$files$unchanged), 1)
+
+  expect_error(
+    orderly_deduplicate_prepare(info),
+    "Can't deduplicate files that have been modified")
+})
+
+
+test_that("don't deduplicate on windows", {
+  mockery::stub(orderly_deduplicate_prepare, "is_windows", TRUE)
+  path <- orderly_example("demo")
+  x <- orderly_deduplicate_info(orderly_config(path))
+  expect_error(
+    orderly_deduplicate_prepare(x),
+    "Deduplication is not supported on windows")
+})
+
+
+test_that("relink basic case", {
+  skip_on_windows()
+  path <- tempfile()
+  from <- file.path(path, "a")
+  to <- file.path(path, "b")
+  dir.create(path)
+  writeLines("a", from)
+  writeLines("b", to)
+  info <- fs::file_info(c(from, to))$inode
+  relink(from, to)
+  expect_true(all(fs::file_info(c(from, to))$inode == info[[2]]))
+})
+
+
+test_that("relink error handling", {
+  skip_on_windows()
+  path <- tempfile()
+  from <- file.path(path, "a")
+  to <- file.path(path, "b")
+  dir.create(path)
+  writeLines("a", from)
+  writeLines("b", to)
+
+  mockery::stub(relink, "fs::link_create",
+                function(...) stop("Some error linking"))
+  info <- fs::file_info(c(from, to))$inode
+  expect_error(relink(from, to), "Some error linking")
+  expect_true(all(fs::file_info(c(from, to))$inode == info))
+})
