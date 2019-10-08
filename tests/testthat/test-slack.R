b907006857966ce3fe791b1d9cd35f176f148fd8
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-10-08 10:18:46 +0100

    Fix tests

--- tests/testthat/test-slack.R
+++ tests/testthat/test-slack.R
@@ -55,7 +55,9 @@ test_that("git information is collected correctly", {
                           id = "20181213-123456-fedcba98",
                           name = "example"),
               git = NULL)
-  d <- slack_data(dat, server_name, server_url, server_is_primary)
+  report_url <- sprintf("%s/reports/%s/%s/", server_url,
+                        dat$meta$name, dat$meta$id)
+  d <- slack_data(dat, server_name, report_url, server_is_primary)
 
   expect_equal(length(d$attachments[[1]]$fields), 1L)
 
@@ -63,7 +65,7 @@ test_that("git information is collected correctly", {
                           id = "20181213-123456-fedcba98",
                           name = "example"),
               git = list(branch = "master", sha_short = "abcdefg"))
-  d <- slack_data(dat, server_name, server_url, server_is_primary)
+  d <- slack_data(dat, server_name, report_url, server_is_primary)
   expect_equal(length(d$attachments[[1]]$fields), 2L)
   expect_equal(d$attachments[[1]]$fields[[2L]],
                list(title = "git", value = "master@abcdefg", short = TRUE))
@@ -73,7 +75,7 @@ test_that("git information is collected correctly", {
                           name = "example"),
               git = list(branch = "master", sha_short = "abcdefg",
                          github_url = "https://github.com/vimc/repo"))
-  d <- slack_data(dat, server_name, server_url, server_is_primary)
+  d <- slack_data(dat, server_name, report_url, server_is_primary)
   expect_equal(
     d$attachments[[1]]$fields[[2]]$value,
     paste0("<https://github.com/vimc/repo/tree/master|master>@",
@@ -91,7 +93,9 @@ test_that("git information works in detached head mode", {
                           name = "example"),
               git = list(branch = NULL, sha_short = "abcdefg",
                          github_url = "https://github.com/vimc/repo"))
-  d <- slack_data(dat, server_name, server_url, server_is_primary)
+  report_url <- sprintf("%s/reports/%s/%s/", server_url,
+                        dat$meta$name, dat$meta$id)
+  d <- slack_data(dat, server_name, report_url, server_is_primary)
   expect_equal(
     d$attachments[[1]]$fields[[2]]$value,
     "(detached)@<https://github.com/vimc/repo/tree/abcdefg|abcdefg>")
@@ -106,8 +110,10 @@ test_that("primary server changes colour", {
                           id = "20181213-123456-fedcba98",
                           name = "example"),
               git = NULL)
-  d1 <- slack_data(dat, server_name, server_url, TRUE)
-  d2 <- slack_data(dat, server_name, server_url, FALSE)
+  report_url <- sprintf("%s/reports/%s/%s/", server_url,
+                        dat$meta$name, dat$meta$id)
+  d1 <- slack_data(dat, server_name, report_url, TRUE)
+  d2 <- slack_data(dat, server_name, report_url, FALSE)
   expect_equal(d1$attachments[[1]]$color, "good")
   expect_equal(d2$attachments[[1]]$color, "warning")
   d2$attachments[[1]]$color <- d1$attachments[[1]]$color
@@ -125,7 +131,9 @@ test_that("sending messages is not a failure", {
                           id = "20181213-123456-fedcba98",
                           name = "example"),
               git = NULL)
-  d <- slack_data(dat, server_name, server_url, server_is_primary)
+  report_url <- sprintf("%s/reports/%s/%s/", server_url,
+                        dat$meta$name, dat$meta$id)
+  d <- slack_data(dat, server_name, report_url, server_is_primary)
 
   slack_url <- "https://httpbin.org/status/403"
   expect_message(do_slack_post_success(slack_url, d),
@@ -173,13 +181,11 @@ test_that("main interface", {
     "    args:",
     sprintf("      path: %s", path),
     "    slack_url: https://httpbin.org/post",
-    "    url: https://example.com",
     "  production:",
     "    driver: orderly::orderly_remote_path",
     "    args:",
     sprintf("      path: %s", path),
     "    slack_url: https://httpbin.org/post",
-    "    url: https://example.com",
     "    primary: true"),
     file.path(path, "orderly_config.yml"))
 
@@ -197,7 +203,7 @@ test_that("main interface", {
   d <- httr::content(r)
   expect_equal(d$json$attachments[[1]]$color, "good") # primary
   expect_equal(d$json$attachments[[1]]$actions[[1]]$url,
-               "https://example.com/reports/example/20181213-123456-fedcba98/")
+               file.path(path, "example", dat$meta$id, fsep = "/"))
 
   config <- withr::with_envvar(
     c("ORDERLY_API_SERVER_IDENTITY" = "testing"),
@@ -237,9 +243,9 @@ test_that("slack payload is correct given actual run data", {
   server_is_primary <- FALSE
   server_name <- "myserver"
 
-  d <- slack_data(dat, server_name, server_url, server_is_primary)
-
-  report_url <- sprintf("%s/reports/%s/%s/", server_url, "minimal", id)
+  report_url <- sprintf("%s/reports/%s/%s/", server_url,
+                        dat$meta$name, dat$meta$id)
+  d <- slack_data(dat, server_name, report_url, server_is_primary)
   git_info <- sprintf("%s@%s", dat$git$branch, dat$git$sha_short)
 
   expect_equal(d$username, "orderly")
