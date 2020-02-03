f2cb2ba7213cac5623cf0da8cfbc628817fa9897
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2020-02-03 15:07:19 +0000

    Add the orderly version to the docker image

--- docker/build-ci.sh
+++ docker/build-ci.sh
@@ -6,6 +6,7 @@ GIT_BRANCH=$(git symbolic-ref --short HEAD)
 REGISTRY=docker.montagu.dide.ic.ac.uk:5000
 NAME=orderly
 PUBLIC_REGISTRY=vimc
+ORDERLY_VERSION=$(grep '^Version: ' DESCRIPTION  | sed 's/Version: *//')
 
 APP_DOCKER_TAG=$REGISTRY/$NAME
 APP_DOCKER_COMMIT_TAG=$REGISTRY/$NAME:$GIT_ID
@@ -14,6 +15,7 @@ APP_DOCKER_BRANCH_TAG=$REGISTRY/$NAME:$GIT_BRANCH
 docker build \
        --build-arg GIT_ID=$GIT_ID \
        --build-arg GIT_BRANCH=$GIT_BRANCH \
+       --build-arg ORDERLY_VERSION=$ORDERLY_VERSION \
        --tag $APP_DOCKER_COMMIT_TAG \
        --tag $APP_DOCKER_BRANCH_TAG \
        -f docker/Dockerfile .
@@ -22,7 +24,9 @@ docker push $APP_DOCKER_BRANCH_TAG
 docker push $APP_DOCKER_COMMIT_TAG
 
 if [ $GIT_BRANCH = "master" ]; then
-    PUBLIC_TAG=$PUBLIC_REGISTRY/$NAME:master
     docker tag $APP_DOCKER_BRANCH_TAG $PUBLIC_TAG
-    docker push $PUBLIC_TAG
+    docker push $PUBLIC_REGISTRY/$NAME:master
+    # Also the versioned images
+    docker push $REGISTRY/$NAME:$ORDERLY_VERSION
+    docker push $PUBLIC_REGISTRY/$NAME:v$ORDERLY_VERSION
 fi
