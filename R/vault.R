08876cc5c4bd3dd2e9378410c3a72735519a4cd8
Wes Hinsley <w.hinsley@imperial.ac.uk>
2020-02-11 10:48:36 +0000

    Fix a couple more to be safe

--- R/vault.R
+++ R/vault.R
@@ -1,9 +1,9 @@
 resolve_secrets <- function(x, config) {
-  if (is.null(config$vault)) {
+  if (is.null(config[['vault']])) {
     return(x)
   }
   loadNamespace("vaultr")
   withr::with_envvar(
     orderly_envir_read(config$root),
-    vaultr::vault_resolve_secrets(x, vault_args = resolve_env(config$vault)))
+    vaultr::vault_resolve_secrets(x, vault_args = resolve_env(config[['vault']])))
 }
