f2dd3a407ebba1a4f59391897dba41d75399bc74
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-03-30 14:58:02 +0100

    Adapt migrations

--- inst/migrate/0.5.5.R
+++ inst/migrate/0.5.5.R
@@ -2,7 +2,7 @@ migrate <- function(data, path, config) {
   if (!is.null(data$meta$connection)) {
     return(migration_result(FALSE, data))
   }
-  d <- recipe_read(path, config, FALSE)
+  d <- orderly_recipe$new(basename(path), config)
   data$meta$connection <- !is.null(d$connection)
   migration_result(TRUE, data)
 }
