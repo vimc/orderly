d69b71292cad290f5e28c83eab6807d097ef3e1b
Robert Ashton <r.ashton@imperial.ac.uk>
2020-05-05 18:38:02 +0100

    Pass instance arg through to run

--- R/remote.R
+++ R/remote.R
@@ -225,12 +225,13 @@ orderly_run_remote <- function(name, parameters = NULL, ref = NULL,
                                open = TRUE, stop_on_error = TRUE,
                                stop_on_timeout = TRUE,
                                progress = TRUE,
-                               root = NULL, locate = TRUE, remote = NULL) {
+                               root = NULL, locate = TRUE, instance = NULL,
+                               remote = NULL) {
   remote <- get_remote(remote, orderly_config_get(root, locate))
   invisible(remote$run(
     name, parameters = parameters, ref = ref,
     timeout = timeout, wait = wait, poll = poll,
-    progress = progress, open = open,
+    progress = progress, open = open, instance = instance,
     stop_on_error = stop_on_error,
     stop_on_timeout = stop_on_timeout))
 }
