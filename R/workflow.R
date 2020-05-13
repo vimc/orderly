d7e66c57ce253fa4c42f4096081cdd50c710c78b
Robert Ashton <r.ashton@imperial.ac.uk>
2020-05-13 16:33:05 +0100

    Impl support for running a workflow

--- R/workflow.R
+++ R/workflow.R
@@ -5,6 +5,7 @@
 ##' @param name Name of workflow to run
 ##'
 ##' @inheritParams orderly_list
+##' @inheritParams orderly_run
 ##'
 ##' @return ID of workflow run
 ##' @export
@@ -14,13 +15,14 @@
 ##'
 ##' # To run most reports, provide the report name (and the path if
 ##' # not running in the working directory, as is the case here):
-##' #id <- orderly::orderly_workflow("my_workflow", root = path)
-orderly_workflow <- function(name, root = NULL, locate = TRUE) {
+##' ids <- orderly::orderly_workflow("my_workflow", root = path)
+orderly_workflow <- function(name, envir = NULL, root = NULL, locate = TRUE,
+                             message = NULL, instance = NULL, remote = NULL) {
   ## Locate file
   config <- orderly_config_get(root, locate)
   workflow <- orderly_workflow_get(name, config)
 
-  workflow$run()
+  workflow$run(envir, message, instance, remote)
 
   ## TODO: Write to DB after run
 }
@@ -50,10 +52,23 @@ workflow <- R6::R6Class(
                                       workflow_path)
     },
 
-    run = function() {
-      ## TODO: Implement run
-      orderly_log("workflow", paste0("Running workflow ", self$workflow_id))
-      stop("Workflow running not yet implemented")
+    run = function(envir = NULL, message = NULL, instance = NULL,
+                   remote = NULL) {
+      orderly_log("workflow", sprintf("Running workflow '%s' with ID '%s'",
+                                      self$workflow_name, self$workflow_id))
+      output <- lapply(self$steps, run_single, envir, self$config, message,
+                       instance, remote)
+      run_ids <- unlist(lapply(output, function(out) {
+        out$value
+      }))
+      out_message <- sprintf("Completed running workflow '%s' with ID '%s'\n",
+                             self$workflow_name, self$workflow_id)
+      no_failed <- length(self$steps) - length(run_ids)
+      if (no_failed > 0) {
+        out_message <- sprintf("%s with %s failure(s)", out_message, no_failed)
+      }
+      orderly_log("workflow", out_message)
+      run_ids
     }
   )
 )
@@ -75,4 +90,26 @@ validate_workflow <- function(raw_yml, config, workflow_name, workflow_path) {
                  workflow_name, paste(squote(msg), collapse = ", ")))
   }
   steps
+}
+
+run_single <- function(report, envir, root, message, instance, remote) {
+  tryCatch({
+    orderly_log("workflow", sprintf("Running report '%s'", report))
+    id <- orderly_run(report, envir = envir, root = root, message = message,
+                      instance = instance, remote = remote)
+    orderly_log("workflow",
+                sprintf("Finished running report '%s'", report))
+    orderly_log("workflow",
+                sprintf("Committing report '%s'", report))
+    orderly_commit(id, root = root)
+    orderly_log("workflow",
+                sprintf("Finished committing report '%s'", report))
+    list(value = id, success = TRUE, error = NULL)
+  },
+  error = function(e) {
+    orderly_log("error",
+                sprintf("Running report '%s' failed with message \n%s",
+                        report, e$message))
+    list(value = NULL, success = FALSE, error = e)
+  })
 }
--- tests/testthat/test-workflow.R
+++ tests/testthat/test-workflow.R
@@ -2,8 +2,80 @@ context("workflow")
 
 test_that("workflow can be run", {
   path <- prepare_orderly_example("demo")
-  expect_error(orderly_workflow("my_workflow", root = path),
-               "Workflow running not yet implemented")
+  mock_new_report_id <- mockery::mock("report_id_1", "report_id_2")
+  mock_random_id <- mockery::mock("workflowid")
+
+  with_mock(
+    "orderly:::new_report_id" = mock_new_report_id,
+    "ids::random_id" = mock_random_id, {
+    output <- evaluate_promise(orderly_workflow("my_workflow", root = path))
+  })
+
+  expect_true("[ workflow   ]  Running workflow 'my_workflow' with ID 'workflowid'\n" %in% output$messages)
+  expect_true("[ workflow   ]  Running report 'minimal'\n" %in% output$messages)
+  expect_true("[ workflow   ]  Finished running report 'minimal'\n" %in% output$messages)
+  expect_true("[ workflow   ]  Committing report 'minimal'\n" %in% output$messages)
+  expect_true("[ workflow   ]  Finished committing report 'minimal'\n" %in% output$messages)
+  expect_true("[ workflow   ]  Running report 'global'\n" %in% output$messages)
+  expect_true("[ workflow   ]  Finished running report 'global'\n" %in% output$messages)
+  expect_true("[ workflow   ]  Committing report 'global'\n" %in% output$messages)
+  expect_true("[ workflow   ]  Finished committing report 'global'\n" %in% output$messages)
+  expect_true("[ workflow   ]  Completed running workflow 'my_workflow' with ID 'workflowid'\n" %in% output$messages)
+
+  expect_equal(output$result, c("report_id_1", "report_id_2"))
+
+  expect_true(file.exists(path_orderly_run_rds(
+    file.path(path, "archive", "minimal", output$result[1]))))
+  expect_true(file.exists(path_orderly_run_rds(
+    file.path(path, "archive", "global", output$result[2]))))
+})
+
+test_that("workflow returns completed IDs if a report fails", {
+  path <- prepare_orderly_example("demo")
+
+  ## Ensure one of the reports fails
+  p <- file.path(path, "src", "minimal", "script.R")
+  txt <- c(readLines(p), "stop('got an error')")
+  writeLines(txt, p)
+
+  mock_new_report_id <- mockery::mock("report_id_1", "report_id_2")
+  mock_random_id <- mockery::mock("workflowid")
+  with_mock(
+    "orderly:::new_report_id" = mock_new_report_id,
+    "ids::random_id" = mock_random_id, {
+      output <- evaluate_promise(orderly_workflow("my_workflow", root = path))
+  })
+
+  expect_true("[ workflow   ]  Running report 'minimal'\n" %in% output$messages)
+  expect_true("[ error      ]  Running report 'minimal' failed with message \ngot an error\n" %in% output$messages)
+  expect_true("[ workflow   ]  Running report 'global'\n" %in% output$messages)
+  expect_true("[ workflow   ]  Completed running workflow 'my_workflow' with ID 'workflowid'\n with 1 failure(s)\n" %in% output$messages)
+
+  expect_equal(output$result, "report_id_2")
+
+  expect_false(file.exists(file.path(path, "archive", "minimal")))
+  expect_true(file.exists(path_orderly_run_rds(
+    file.path(path, "archive", "global", output$result[1]))))
+})
+
+test_that("envir, message, instance, remote get passed to orderly_run", {
+  path <- prepare_orderly_example("demo")
+  mock_orderly_run <- mockery::mock("report_id_1", "report_id_2")
+  with_mock("orderly::orderly_run" = mock_orderly_run, {
+    orderly_workflow("my_workflow", root = path, envir = "envir",
+                     message = "message", instance = "instance",
+                     remote = "remote")
+  })
+  mockery::expect_called(mock_orderly_run, 2)
+  args <- mockery::mock_args(mock_orderly_run)
+  expect_equal(args[[1]]$envir, "envir")
+  expect_equal(args[[1]]$message, "message")
+  expect_equal(args[[1]]$instance, "instance")
+  expect_equal(args[[1]]$remote, "remote")
+  expect_equal(args[[2]]$envir, "envir")
+  expect_equal(args[[2]]$message, "message")
+  expect_equal(args[[2]]$instance, "instance")
+  expect_equal(args[[2]]$remote, "remote")
 })
 
 test_that("orderly_workflow throws an error if it cannot locate workflow", {
@@ -17,7 +89,7 @@ test_that("orderly_workflow throws an error if it cannot locate workflow", {
 test_that("orderly_workflow throws error if report does not exist", {
   path <- prepare_orderly_example("workflow", testing = TRUE)
   expect_error(
-    orderly_workflow("missing_report", path),
+    orderly_workflow("missing_report", root = path),
     paste0("Cannot run workflow 'missing_report' as reports missing: ",
            "'missing', 'missing2'"))
 })
@@ -25,17 +97,17 @@ test_that("orderly_workflow throws error if report does not exist", {
 test_that("orderly_workflow throws error if steps do not exist", {
   path <- prepare_orderly_example("workflow", testing = TRUE)
   expect_error(
-    orderly_workflow("missing_steps", path),
+    orderly_workflow("missing_steps", root = path),
     "Fields missing from .+/workflows/missing_steps.yml: steps")
 })
 
 test_that("orderly_workflow throws error if steps are misconfigured", {
   path <- prepare_orderly_example("workflow", testing = TRUE)
   expect_error(
-    orderly_workflow("missing_name", path),
+    orderly_workflow("missing_name", root = path),
     "Fields missing from .+/workflows/missing_name.yml:steps: name")
   expect_error(
-    orderly_workflow("broken_steps", path),
+    orderly_workflow("broken_steps", root = path),
     "Unknown fields in .+/workflows/broken_steps.yml:steps: field")
 })
 
