ccd9ee032f1db13ca88d74dde70c84be661ae190
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-06-08 18:32:25 +0100

    Add some basic docs

--- R/task.R
+++ R/task.R
@@ -1,12 +1,89 @@
-orderly_task_pack <- function(dest, name = NULL, parameters = NULL,
+##' Pack up and run orderly tasks to run elsewhere. By using these
+##' functions you can safely copy all requirements of an orderly
+##' report into a portable archive and run them on another machine
+##' (perhaps a cluster or HPC), then import the completed archive into
+##' your orderly tree. There is considerable overhead to using these
+##' functions (mostly due to transport costs) so they are intended
+##' primarily for very computationally demanding patterns.
+##'
+##' @title Pack and run orderly "tasks"
+##'
+##' @param path A path, whose interpretation depends on the function:
+##'
+##' \describe{
+##'
+##' \item{\code{orderly_task_pack}}{A directory to save tasks to.  If
+##'   it does not exist it will be created for you.}
+##'
+##' \item{\code{orderly_task_run}}{The path to the packed task (a zip
+##'   file created by \code{orderly_task_pack})}
+##'
+##' \item{\code{orderly_task_run}}{The path to the run task task (a
+##'   zip file created by \code{orderly_task_run}}
+##'
+##' \item{\code{orderly_task_list}}{The path to a directory that might
+##'   contain either incomplete or complete tasks (created by either
+##'   \code{orderly_task_pack} or \code{orderly_task_run})}
+##'
+##' }
+##'
+##' @inheritParams orderly_run
+##'
+##' @return For \code{orderly_task_pack} and \code{orderly_task_run},
+##'   a list with elements \code{path} (the pth to the packed task)
+##'   and \code{id} (its orderly id).  For \code{orderly_task_list} a
+##'   data.frame with key information about the tasks (id, name,
+##'   parameters, status, time).  The function
+##'   \code{orderly_task_import} is called for its side effect only
+##'   and does not return anything useful.
+##'
+##' @export
+##' @examples
+##' path <- orderly::orderly_example("minimal")
+##'
+##' # A working directory to export tasks to:
+##' workdir <- tempfile()
+##'
+##' # Pack up the "example" report to go:
+##' res <- orderly::orderly_task_pack(workdir, "example", root = path)
+##'
+##' # The return value is a list with the id and the path to the zip
+##' # file created:
+##' res
+##'
+##' # A list of tasks saved to this directory and their status
+##' orderly::orderly_task_list(workdir)
+##'
+##' # Run the task (this would ordinarily be done on another computer)
+##' zip <- orderly::orderly_task_run(res$path, workdir)
+##' zip
+##'
+##' # The status has now been updated to reflect the status
+##' orderly::orderly_task_list(workdir)
+##'
+##' # We can import this into the orderly tree
+##' orderly::orderly_task_import(zip, root = path)
+##'
+##' # This has now been included in your orderly archive and the
+##' # workdir can be safely deleted
+##' unlink(workdir, recursive = TRUE)
+##' orderly::orderly_list_archive(path)
+orderly_task_pack <- function(path, name = NULL, parameters = NULL,
                               envir = NULL, root = NULL, locate = NULL,
                               message = NULL, instance = NULL,
                               remote = NULL, tags = NULL) {
   version <- orderly_version$new(name, root, locate)
-  version$task_pack(dest, parameters, instance, envir, remote = NULL)
+  version$task_pack(path, parameters, instance, envir, remote = NULL)
 }
 
 
+##' @param workdir The path in which to run tasks.  If it does not
+##'   exist it will be created for you.  The completed task will be
+##'   saved in this directory as \code{<id>.zip}.
+##'
+##' @inheritParams orderly_run
+##' @rdname orderly_task_pack
+##' @export
 orderly_task_run <- function(path, workdir = tempfile(), echo = TRUE,
                              envir = NULL) {
   dir_create(workdir)
@@ -44,6 +121,10 @@ orderly_task_run <- function(path, workdir = tempfile(), echo = TRUE,
   list(id = id, path = zip)
 }
 
+
+
+##' @rdname orderly_task_pack
+##' @export
 orderly_task_import <- function(path, root = NULL, locate = NULL) {
   config <- orderly_config_get(root, locate)
 
@@ -78,14 +159,16 @@ orderly_task_import <- function(path, root = NULL, locate = NULL) {
 }
 
 
+##' @rdname orderly_task_pack
+##' @export
 orderly_task_list <- function(path) {
   f <- function(p) {
     info <- orderly_task_info(p)
     status <- orderly_task_status(p)
     data_frame(id = info$id,
-               status = status,
                name = info$name,
                parameters = I(list(info$parameters)),
+               status = status,
                time = info$preflight_info$time)
   }
 
