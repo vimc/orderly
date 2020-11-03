ae681ae15a50542686ad60742c0c55bdd3367500
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-11-03 20:19:41 +0000

    Fix syntax error

--- tests/testthat/test-util-assert.R
+++ tests/testthat/test-util-assert.R
@@ -54,7 +54,7 @@ test_that("assert_type", {
 })
 
 test_that("assert_file_exists", {
-  path <- tempfile(tmpdir = normalizePath(tempdir(), mustWork = TRUE)
+  path <- tempfile(tmpdir = normalizePath(tempdir(), mustWork = TRUE))
   expect_error(assert_file_exists(path), "File does not exist")
   writeLines(character(0), path)
   expect_silent(assert_file_exists(path))
