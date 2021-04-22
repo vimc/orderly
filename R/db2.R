6720db09b14f4631c8cf15f1e4ea63ae5481e1aa
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-04-22 16:28:13 +0100

    Allow all custom fields to be empty

--- R/db2.R
+++ R/db2.R
@@ -283,12 +283,14 @@ report_data_import <- function(con, name, id, config) {
   if (!is.null(dat_rds$meta$extra_fields)) {
     custom <- vcapply(dat_rds$meta$extra_fields, function(x) as.character(x))
     custom <- custom[!is.na(custom)]
-    report_version_custom_fields <- data_frame(
-      report_version = id,
-      key = names(custom),
-      value = unname(custom))
-    DBI::dbWriteTable(con, "report_version_custom_fields",
-                      report_version_custom_fields, append = TRUE)
+    if (length(custom) > 0) {
+      report_version_custom_fields <- data_frame(
+        report_version = id,
+        key = names(custom),
+        value = unname(custom))
+      DBI::dbWriteTable(con, "report_version_custom_fields",
+                        report_version_custom_fields, append = TRUE)
+    }
   }
 
   if (!is.null(dat_rds$meta$view)) {
